e0b0683c2c784c6424c69276b6e59e1a04a7b28f
Khaylac <luizantoniomotaluz@gmail.com>
2026-08-22 00:18:51 -0300

    Update reports.js

--- reports.js
+++ reports.js
@@ -1,7 +1,7 @@
 const reports = [
 
     {
-        name: "Pedido de Venda A4 TESTE",
+        name: "Pedido de Venda A4 1",
         category: "Sales",
         description: "Sales order report in A4 format.",
         folder: "pedido-de-venda-a4-1"
