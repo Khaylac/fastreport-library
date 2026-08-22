d4031426597fee732faeab0ba1fc6fe9688cfa14
Khaylac <luizantoniomotaluz@gmail.com>
2026-08-22 11:10:54 -0300

    Test multiple report card layouts

--- reports.js
+++ reports.js
@@ -7,6 +7,15 @@ const reports = [
         version: "1.0",
         updated: "22/08/2026",
         folder: "pedido-de-venda-a4-1"
+    },
+
+    {
+        name: "Long Description Test",
+        category: "Inventory",
+        description: "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
+        version: "1.0",
+        updated: "22/08/2026",
+        folder: "pedido-de-venda-a4-1"
     }
 
 ];
