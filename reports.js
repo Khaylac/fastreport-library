8afac4ce439923e9093badfd7a3a6840565435a0
Khaylac <luizantoniomotaluz@gmail.com>
2026-08-22 11:00:43 -0300

    Add report metadata

--- reports.js
+++ reports.js
@@ -4,6 +4,8 @@ const reports = [
         name: "Pedido de Venda A4 1",
         category: "Sales",
         description: "Sales order report in A4 format.",
+        version: "1.0",
+        updated: "22/08/2026",
         folder: "pedido-de-venda-a4-1"
     }
 
