981154f39a0834339f7056803b5d7be4b8ffd7ee
Khaylac <luizantoniomotaluz@gmail.com>
2026-08-22 11:24:28 -0300

    Prepare report data for detail pages

--- reports.js
+++ reports.js
@@ -1,39 +1,146 @@
 const reports = [
 
     {
+        id: "pedido-de-venda-a4-1",
+
         name: "Pedido de Venda A4 1",
+
         category: "Sales",
-        description: "Sales order report in A4 format.",
+
+        description:
+            "Sales order report in A4 format.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "pedido-de-venda-a4-1"
+
+        folder: "pedido-de-venda-a4-1",
+
+
+        features: [
+            "A4 page format",
+            "Sales order information",
+            "Ready for FastReport"
+        ],
+
+
+        requirements: [
+            "FastReport 2022.3 or newer",
+            "Compatible database structure"
+        ],
+
+
+        parameters: [
+            "Sales order",
+            "Company"
+        ]
+
     },
 
-    {
-        name: "Long Description Test ",
-        category: "Inventory",
-        description: "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
-        version: "1.0",
-        updated: "22/08/2026",
-        folder: "pedido-de-venda-a4-1"
-    },
 
     {
-        name: "Long Description Test 2",
+        id: "long-description-test",
+
+        name: "Long Description Test",
+
         category: "Inventory",
-        description: "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
+
+        description:
+            "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "pedido-de-venda-a4-1"
+
+        folder: "pedido-de-venda-a4-1",
+
+
+        features: [
+            "Test feature one",
+            "Test feature two",
+            "Test feature three"
+        ],
+
+
+        requirements: [
+            "Test requirement"
+        ],
+
+
+        parameters: [
+            "Test parameter"
+        ]
+
     },
 
+
     {
-        name: "Long Description Test 3",
-        category: "Inventory",
-        description: "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
+        id: "test-report-3",
+
+        name: "Test Report 3",
+
+        category: "Production",
+
+        description:
+            "A temporary report used to test the layout and future functionality of the FastReport Library.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "pedido-de-venda-a4-1"
+
+        folder: "pedido-de-venda-a4-1",
+
+
+        features: [
+            "Temporary feature",
+            "Another temporary feature"
+        ],
+
+
+        requirements: [
+            "Test environment"
+        ],
+
+
+        parameters: [
+            "Date range"
+        ]
+
+    },
+
+
+    {
+        id: "test-report-4",
+
+        name: "Test Report 4",
+
+        category: "Finance",
+
+        description:
+            "Another temporary report used to populate the second row and test the overall layout of the report library.",
+
+        version: "1.0",
+
+        updated: "22/08/2026",
+
+        folder: "pedido-de-venda-a4-1",
+
+
+        features: [
+            "Temporary feature",
+            "Layout testing"
+        ],
+
+
+        requirements: [
+            "Test environment"
+        ],
+
+
+        parameters: [
+            "Company"
+        ]
+
     }
 
 ];
