1b1bd4ef5e28fdfd3c7ec4954d708241b8ea5588
Khaylac <luizantoniomotaluz@gmail.com>
2026-08-22 14:44:57 -0300

    Update reports.js

--- reports.js
+++ reports.js
@@ -2,34 +2,41 @@ const reports = [
 
     {
         id: "pedido-de-venda-a4-1",
-        name: "Pedido de Venda A4 1",
-        category: "Pedidos de Venda",
-        description:
-            "Layout A4 para tela pedido de venda.",
-        version: "1.0",
-        updated: "22/08/2026",
-        folder: "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
-        images: [
-            "preview.png",
-            "preview-2.png"
-        ],
 
+        name: "Pedido de Venda A4 1",
+
+        screen: "Pedidos de Venda",
+
+        type: "Controle de Impressos",
+
+        description:
+            "Sales order report in A4 format.",
+
+        version: "1.0",
+
+        updated: "22/08/2026",
+
+        folder:
+            "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
+
+        images: [
+            "preview.png"
+        ],
 
         features: [
-            "Formato de Impressão: A4",
-            "Informações básicas do pedido de venda"
+            "A4 page format",
+            "Sales order information",
+            "Ready for FastReport"
         ],
-
 
         requirements: [
-            "FastReport 2022.3+",
-            "Firebird 3.0+"
+            "FastReport 2022.3 or newer",
+            "Compatible database structure"
         ],
 
-
         parameters: [
-            "Pedidos de Venda",
-            "Layouts"
+            "Sales order",
+            "Company"
         ]
 
     },
@@ -37,13 +44,23 @@ const reports = [
 
     {
         id: "long-description-test",
+
         name: "Long Description Test",
-        category: "Inventory",
+
+        screen: "Produtos",
+
+        type: "Controle de Impressos",
+
         description:
             "This is a deliberately much longer description used to test how the report cards behave when different reports contain different amounts of text in their description section.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
+
+        folder:
+            "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
+
         images: [
             "preview.png"
         ],
@@ -67,18 +84,27 @@ const reports = [
 
     {
         id: "test-report-3",
+
         name: "Test Report 3",
-        category: "Production",
+
+        screen: "Ordens de Serviço",
+
+        type: "Controle de Impressos",
+
         description:
             "A temporary report used to test the layout and future functionality of the FastReport Library.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
+
+        folder:
+            "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
 
         images: [
             "preview.png"
         ],
-        
+
         features: [
             "Temporary feature",
             "Another temporary feature"
@@ -97,13 +123,22 @@ const reports = [
 
     {
         id: "test-report-4",
+
         name: "Test Report 4",
-        category: "Finance",
+
+        screen: "Pedidos de Venda",
+
+        type: "Controle de Impressos",
+
         description:
             "Another temporary report used to populate the second row and test the overall layout of the report library.",
+
         version: "1.0",
+
         updated: "22/08/2026",
-        folder: "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
+
+        folder:
+            "controle-de-impressos/pedidos-de-venda/pedido-de-venda-a4-1",
 
         images: [
             "preview.png"
